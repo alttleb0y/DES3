800fbcdb8d82f3438fb63b819427490bb4725a05
Your GitHub Username <your-email@example.com>
2026-03-19 13:55:59 +0700

    Initialize

---
+++ S6.v
@@ -0,0 +1,73 @@
+module S6 (in, out);
+	input [5:0] in;
+	output reg [3:0] out;
+	
+	always @(*) begin
+		case (in)
+			6'd0 : out = 4'd12;
+			6'd1 : out = 4'd10;
+			6'd2 : out = 4'd1;
+			6'd3 : out = 4'd15;
+			6'd4 : out = 4'd10;
+			6'd5 : out = 4'd4;
+			6'd6 : out = 4'd15;
+			6'd7 : out = 4'd2;
+			6'd8 : out = 4'd9;
+			6'd9 : out = 4'd7;
+			6'd10 : out = 4'd2;
+			6'd11 : out = 4'd12;
+			6'd12 : out = 4'd6;
+			6'd13 : out = 4'd9;
+			6'd14 : out = 4'd8;
+			6'd15 : out = 4'd5;
+			6'd16 : out = 4'd0;
+			6'd17 : out = 4'd6;
+			6'd18 : out = 4'd13;
+			6'd19 : out = 4'd1;
+			6'd20 : out = 4'd3;
+			6'd21 : out = 4'd13;
+			6'd22 : out = 4'd4;
+			6'd23 : out = 4'd14;
+			6'd24 : out = 4'd14;
+			6'd25 : out = 4'd0;
+			6'd26 : out = 4'd7;
+			6'd27 : out = 4'd11;
+			6'd28 : out = 4'd5;
+			6'd29 : out = 4'd3;
+			6'd30 : out = 4'd11;
+			6'd31 : out = 4'd8;
+			6'd32 : out = 4'd9;
+			6'd33 : out = 4'd4;
+			6'd34 : out = 4'd14;
+			6'd35 : out = 4'd3;
+			6'd36 : out = 4'd15;
+			6'd37 : out = 4'd2;
+			6'd38 : out = 4'd5;
+			6'd39 : out = 4'd12;
+			6'd40 : out = 4'd2;
+			6'd41 : out = 4'd9;
+			6'd42 : out = 4'd8;
+			6'd43 : out = 4'd5;
+			6'd44 : out = 4'd12;
+			6'd45 : out = 4'd15;
+			6'd46 : out = 4'd3;
+			6'd47 : out = 4'd10;
+			6'd48 : out = 4'd7;
+			6'd49 : out = 4'd11;
+			6'd50 : out = 4'd0;
+			6'd51 : out = 4'd14;
+			6'd52 : out = 4'd4;
+			6'd53 : out = 4'd1;
+			6'd54 : out = 4'd10;
+			6'd55 : out = 4'd7;
+			6'd56 : out = 4'd1;
+			6'd57 : out = 4'd6;
+			6'd58 : out = 4'd13;
+			6'd59 : out = 4'd0;
+			6'd60 : out = 4'd11;
+			6'd61 : out = 4'd8;
+			6'd62 : out = 4'd6;
+			6'd63 : out = 4'd13;
+		endcase
+	end
+endmodule
